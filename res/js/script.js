7ded7dad260d5522407fd408039391c923aaa5df
migdress <miguelmorcillo@unicauca.edu.co>
2017-03-15 09:56:18 -0500

    Undestood correctyly the base64, including padding.
    * Added new textarea to show 6bits binary groups in practiceBase64
    * Pending to update theoryBase64 and finish practiceBase64

--- res/js/script.js
+++ res/js/script.js
@@ -66,10 +66,12 @@ function encodeBase64Extended(){
 	var input = document.getElementById("input").value;
 	var charCodeArray = convertStringToCharCodeArray(input);
 	document.getElementById("output1").innerHTML = charCodeArray.toString();
-	var binaryString = convertCharCodeArrayToBinaryString(charCodeArray);
-	document.getElementById("output2").innerHTML = binaryString;
-	var base64String = convertBinaryStringToBase64String(binaryString.replace(/,/g,""));
-	document.getElementById("output3").innerHTML = base64String;
+	var binary8bitString = convertCharCodeArrayTo8BinaryString(charCodeArray);
+	document.getElementById("output2").innerHTML = binary8bitString;
+	var binary6bitString = convertBinary8StringToBinary6String(binary8bitString.replace(/,/g,""));
+	document.getElementById("output3").innerHTML = binary6bitString;
+	var base64String = 	convertBinary6StringToBase64(binary6bitString.replace(/,/g,""));
+	document.getElementById("output4").innerHTML = base64String;
 }
 
 function convertToBinary(number){
@@ -91,13 +93,35 @@ function convertBinaryToDecimal(binary){
 	return parseInt(binary, 2);
 }
 
-function convertCharCodeArrayToBinaryString(array){
+function convertCharCodeArrayTo8BinaryString(array){
 	var binArray = [];
 	for(var i=0;i<array.length;i++){
 		binArray[i] = complete8bits(convertToBinary(array[i]));
 		console.log("converted index="+i+" "+binArray[i]);
 	}
 	return binArray.toString();	
+}
+function convertBinary8StringToBinary6String(binary8String){
+	var start = 0;
+	var end = 6;
+	var binary6bits = "";
+	var result = [];
+	var rest = binary8String.length % 24;
+	console.log("Converting 8bit string to 6 bit string...");
+	console.log("initial length = "+binary8String.length);
+	console.log("rest = "+rest);
+	for(var i=0;i<(24-rest);i++){
+		binary8String = binary8String +"0";
+	}
+	console.log("posterior length = "+binary8String.length);
+	for(var i=0; i<(binary8String.length/6);i++){
+		binary6bits = binary8String.slice(start,end);
+		console.log(binary6bits);
+		result[i] = binary6bits;
+		start = start +6;
+		end = end + 6;
+	}
+	return result.toString();
 }
 
 function convertStringToCharCodeArray(string){
@@ -116,7 +140,7 @@ function complete8bits(binary){
 	return binary;
 }
 
-function convertBinaryStringToBase64String(binaryString){
+function convertBinary6StringToBase64String(binary6String){
 	var base64Alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","/"];
 	console.log("alphabet length: "+base64Alphabet.length);
 	var start = 0;
@@ -124,16 +148,19 @@ function convertBinaryStringToBase64String(binaryString){
 	var binary6bits="";
 	var result = "";
 	var decimal = 0;
-	var rest = binaryString.length % 6;
-	console.log("initial length of string: "+binaryString.length);
-	console.log("rest: "+rest);
-	for(var i=0;i<rest;i++){
-		binaryString = binaryString +"0";
-	}
-	console.log("posterior length of string: "+binaryString.length);
-	for(var i=0; i<(binaryString.length/6);i++){
-		binary6bits = binaryString.slice(start,end);
+	var last2bitsFromPrevious = "";
+	console.log("length of string: "+binary6String.length);
+
+	for(var i=0; i<(binary6String.length/6);i++){
+		binary6bits = binary6String.slice(start,end);
 		console.log("from "+start+" to "+end+" group "+i+" :"+binary6bits+" length: "+binary6bits.length);
+		if(i>0){
+			last2bitsFromPrevious = (binary6String.slice(start-6,end-6)).slice(4,6);
+			if(binary6bits == "000000" && last2bitsFromPrevious == "00"){
+				result = result +"=";
+				continue;
+			}
+		}
 		decimal = convertBinaryToDecimal(binary6bits);
 		console.log("decimal: "+decimal);
 		result = result + base64Alphabet[decimal];
